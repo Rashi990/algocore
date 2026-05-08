3adf8c5a933a4608474e1569ed0ac2375bd6dbaa
Rashi990 <rashinilakshika2020@gmail.com>
2026-05-08 13:25:15 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/algocore/algocore/AlgocoreApplication.java
@@ -0,0 +1,13 @@
+package com.algocore.algocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgocoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgocoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algocore/algocore/AlgocoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algocore.algocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgocoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
